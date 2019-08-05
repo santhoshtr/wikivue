9830f6c7466eea79c4514f2a7444a554de42b717
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2019-08-05 23:07:23 +0530

    Add missing BottomNav

---
+++ src/components/BottomNav.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-bottom-navigation
+    v-model="bottomNav"
+    grow
+    app
+    hide-on-scroll
+    shift
+    horizontal
+    scroll-threshold="500"
+  >
+    <v-btn value="recent">
+      <span>Recent</span>
+      <v-icon>history</v-icon>
+    </v-btn>
+
+    <v-btn value="bookmark">
+      <span>Bookmark</span>
+      <v-icon>bookmark</v-icon>
+    </v-btn>
+
+    <v-btn value="nearby">
+      <span>Nearby</span>
+      <v-icon>place</v-icon>
+    </v-btn>
+    <v-btn value="share">
+      <span>Share</span>
+      <v-icon>share</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+
+
+<script>
+export default {
+  name: 'BottomNav',
+  data () {
+      return {
+        bottomNav: 'recent',
+      }
+    }
+}
+</script>
