833d7bf390b1a37cb9ce39100420f0a6e12f8900
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2019-08-08 16:34:56 +0530

    Rewrite all wiki URLS

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -42,7 +42,7 @@
             v-for="section in sections"
             :key="section.id"
             :id="section.anchor"
-            v-html="section.html"
+            v-html=" template(section.html)"
             class="my-4"
           />
           <div
@@ -58,9 +58,10 @@
 </template>
 
 <script>
+
 import TOC from "../components/TOC";
 import ArticleHeader from "../components/ArtcleHeader";
-import { mapGetters, mapState, mapMutations } from 'vuex';
+import { mapGetters, mapState, mapMutations } from "vuex";
 
 export default {
   name: "Article",
@@ -71,18 +72,20 @@ export default {
   data: () => ({
     error: null,
     activeToc: [],
-    bannerImage:"",
+    bannerImage: ""
   }),
   computed: {
     ...mapState({
-      loaded:state => state.article.loadingStatus==='success'||state.article.loadingStatus==='failure',
+      loaded: state =>
+        state.article.loadingStatus === "success" ||
+        state.article.loadingStatus === "failure",
       sections: state => state.article.sections,
       toc: state => state.article.toc,
       displaytitle: state => state.article.title,
       description: state => state.article.description,
       lastmodifier: state => state.article.history.lastmodifier,
       lastmodified: state => state.article.history.lastmodified,
-      contentLanguage: state => state.app.contentLanguage,
+      contentLanguage: state => state.app.contentLanguage
     }),
     title: function() {
       return this.$route.params.title || "";
@@ -93,25 +96,53 @@ export default {
       this.loadArticle();
     }
   },
-  mounted: function(){
+  mounted: function() {
     this.loadArticle();
   },
   methods: {
-    loadArticle(){
-      if(this.$route.meta.random){
-          this.$store.dispatch('article/fetch', {language:this.contentLanguage})
-      }else{
-          this.$store.dispatch('article/fetch', {title:this.title, language:this.contentLanguage})
+    loadArticle() {
+      if (this.$route.meta.random) {
+        this.$store.dispatch("article/fetch", {
+          language: this.contentLanguage
+        });
+      } else {
+        this.$store.dispatch("article/fetch", {
+          title: this.title,
+          language: this.contentLanguage
+        });
       }
+    },
+    template(section) {
+      var wrapper = document.createElement("div");
+      wrapper.innerHTML = section;
+      var links = wrapper.querySelectorAll("a[href]");
+      for (let l = 0; l < links.length; l++) {
+        let link = links[l];
+        for (let i = 0; i < link.attributes.length; i++) {
+          let attribute = link.attributes[i];
+          if (attribute.name === "href") {
+            link.setAttribute(
+              attribute.name,
+              attribute.value.replace(
+                "/wiki/",
+                `/article/${this.contentLanguage}/`
+              )
+            );
+            continue;
+          }
+          link.setAttribute(attribute.name, attribute.value);
+        }
+      }
+      return wrapper.innerHTML;
     }
   }
 };
 </script>
 
 <style>
-
 article {
-  font-family: "Libertinus Serif", "Gentium", Georgia, Cambria, "Times New Roman", Times, serif;
+  font-family: "Libertinus Serif", "Gentium", Georgia, Cambria,
+    "Times New Roman", Times, serif;
 }
 
 .mw-ref {
@@ -139,13 +170,24 @@ p {
   font-size: 18px;
 }
 
-article, aside, details, figcaption, figure, footer, header, main, menu, nav, section, summary,table.wikitable {
-    display: block;
-    overflow: auto;
+article,
+aside,
+details,
+figcaption,
+figure,
+footer,
+header,
+main,
+menu,
+nav,
+section,
+summary,
+table.wikitable {
+  display: block;
+  overflow: auto;
 }
 
 figure {
-    margin: 10px 0;
+  margin: 10px 0;
 }
-
 </style>
