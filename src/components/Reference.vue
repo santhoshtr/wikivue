d1f7a1b888a8ce76bc4895f45af4440f7f24ee17
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2019-08-09 14:47:17 +0530

    Add reference support - for real

---
+++ src/components/Reference.vue
@@ -0,0 +1,56 @@
+<template v-if="$vuetify.breakpoint.smAndUp">
+  <v-bottom-sheet
+    hide-overlay
+    v-model="shown"
+  >
+    <v-sheet
+      height="200px"
+    >
+      <v-layout
+        align-center
+        justify-center
+        row
+        fill-height
+        class="reference-content"
+      >
+        <v-flex
+          xs12
+          sm12
+          md6
+          lg6
+        >
+          <p
+            v-html="reference"
+          />
+        </v-flex>
+      </v-layout>
+    </v-sheet>
+  </v-bottom-sheet>
+</template>
+
+<script>
+export default {
+  name: "Reference",
+  data: () => ({
+      shown: false,
+  }),
+  props: {
+    reference: {
+      type: String,
+      default: null
+    }
+  },
+  watch:{
+      reference:function(){
+          if(this.reference){
+          this.shown=true
+          }
+      }
+  }
+};
+</script>
+<style>
+.reference-content {
+    overflow-y: auto;
+}
+</style>
