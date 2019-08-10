1e3db60cf28e6c7500fc73512c64ec2a88ab360b
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2019-08-10 10:55:48 +0530

    PWA: caching for wikipedia apis

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,21 @@
-module.exports = {}
+module.exports = {
+    pwa: {
+        // configure the workbox plugin
+        workboxOptions: {
+            runtimeCaching: [ {
+                urlPattern: new RegExp('/.*(?:wikipedia)\.org.*$/'),
+                handler: 'cacheFirst',
+                options: {
+                    cacheName: 'Wikipedia-api',
+                    expiration: {
+                        maxEntries: 1000,
+                        maxAgeSeconds: 24 * 60 * 60, // A day
+                    },
+                    cacheableResponse: {
+                        statuses: [0, 200]
+                    }
+                }
+            }]
+        }
+    }
+}
