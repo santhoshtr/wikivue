764ea3ca4a8d7bceeeb7a06f6d7c86e7a687dc51
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2019-08-10 11:56:36 +0530

    Cache images

--- vue.config.js
+++ vue.config.js
@@ -3,13 +3,27 @@ module.exports = {
         // configure the workbox plugin
         workboxOptions: {
             runtimeCaching: [ {
-                urlPattern: new RegExp('^https://.*\.wikipedia\.org/'),
+                urlPattern: new RegExp('^https://.*\.wikipedia\.org/api/'),
                 handler: 'cacheFirst',
                 options: {
                     cacheName: 'Wikipedia-api',
                     expiration: {
                         maxEntries: 1000,
                         maxAgeSeconds: 24 * 60 * 60, // A day
+                    },
+                    cacheableResponse: {
+                        statuses: [0, 200]
+                    }
+                }
+            },
+            {
+                urlPattern: new RegExp('^https://upload\.wikimedia\.org/wikipedia/commons/'),
+                handler: 'cacheFirst',
+                options: {
+                    cacheName: 'Wikipedia-commons',
+                    expiration: {
+                        maxEntries: 10000,
+                        maxAgeSeconds: 7 * 24 * 60 * 60, // A week
                     },
                     cacheableResponse: {
                         statuses: [0, 200]
