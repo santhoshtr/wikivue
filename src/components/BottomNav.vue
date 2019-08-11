46a3035215e787410520ae1cc3d6fb973fb62c72
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2019-08-11 11:57:48 +0530

    Add toc drawer in bottom Navigation

--- src/components/BottomNav.vue
+++ src/components/BottomNav.vue
@@ -1,41 +1,85 @@
 <template>
-  <v-bottom-navigation
-    v-model="bottomNav"
-    grow
-    app
-    hide-on-scroll
-  >
-    <v-btn value="recent">
-      <v-icon>history</v-icon>
-    </v-btn>
+  <v-sheet>
+    <v-bottom-navigation
+      v-model="bottomNav"
+      grow
+      app
+      hide-on-scroll
+    >
+      <v-btn value="recent">
+        <v-icon>history</v-icon>
+      </v-btn>
 
-    <v-btn value="bookmark">
-      <v-icon>bookmark</v-icon>
-    </v-btn>
+      <v-btn value="bookmark">
+        <v-icon>bookmark</v-icon>
+      </v-btn>
 
-    <v-btn value="languages">
-      <v-badge right>
-        <span slot="badge">{{ $store.state.article.languagecount }}</span>
-        <v-icon>translate</v-icon>
-      </v-badge>
-    </v-btn>
+      <v-btn value="languages">
+        <v-badge right>
+          <span slot="badge">{{ $store.state.article.languagecount }}</span>
+          <v-icon>translate</v-icon>
+        </v-badge>
+      </v-btn>
 
-    <v-btn value="share">
-      <v-icon>share</v-icon>
-    </v-btn>
-  </v-bottom-navigation>
+      <v-btn
+        v-show="sharable"
+        value="sharable"
+      >
+        <v-icon>share</v-icon>
+      </v-btn>
+      <v-btn
+        v-show="$vuetify.breakpoint.smAndDown"
+        value="tocbutton"
+        @click="showToc"
+      >
+        <v-icon>list</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+    <v-navigation-drawer
+      v-model="tocShown"
+      right
+      hide-overlay
+      clipped
+      app
+      class="toc-drawer"
+    >
+      <table-of-contents :toc="toc" />
+    </v-navigation-drawer>
+  </v-sheet>
 </template>
 
 
 <script>
+import TableOfContents from "./TOC";
+
 export default {
   name: "BottomNav",
+  components: {
+    TableOfContents,
+  },
   data() {
     return {
-      bottomNav: "recent"
+      bottomNav: "recent",
+      tocShown: false,
+       items: [
+        { title: 'Click Me' },
+
+        { title: 'Click Me 2' },
+      ],
     };
   },
+  computed:{
+    sharable(){
+      return navigator.share;
+    },
+    toc(){
+      return this.$store.state.article.toc
+    }
+  },
   methods: {
+    showToc(){
+      this.tocShown=true;
+    },
     share() {
       if (navigator.share) {
         navigator
@@ -51,3 +95,8 @@ export default {
   }
 };
 </script>
+<style>
+.toc-drawer .toc {
+  max-width: 100%;
+}
+</style>
