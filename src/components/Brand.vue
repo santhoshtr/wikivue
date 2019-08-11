46d0fe38a29f7c7df129199ad8cd53c5677b4d35
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2019-08-11 21:35:59 +0530

    Replace wikipedia logo by text and required styling using Libertinus

--- src/components/Brand.vue
+++ src/components/Brand.vue
@@ -1,11 +1,10 @@
-<template v-if="$vuetify.breakpoint.smAndUp">
-  <v-img
-    contain
-    :aspect-ratio="1/1"
-    max-height="20px"
-    position="left center"
-    src="../assets//Wikipedia_wordmark.png"
-  />
+<template>
+  <v-toolbar-title
+    class="wikipedia"
+    v-show="$vuetify.breakpoint.smAndUp"
+  >
+    <span class="cap">W</span><span class="smallcap">ikipedi</span><span class="cap">A</span>
+  </v-toolbar-title>
 </template>
 
 <script>
@@ -13,3 +12,13 @@ export default {
   name: 'Brand'
 }
 </script>
+<style lang="less" >
+.wikipedia {
+  padding-right: 20px;
+  font-family: "Libertinus serif" !important;
+  font-feature-settings: "salt";
+  .smallcap {
+    font-variant: common-ligatures small-caps;
+  }
+}
+</style>
