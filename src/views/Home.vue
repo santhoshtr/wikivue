9fe4d2095938e56d64fa8ab11b5cf8888534f1ec
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2019-09-09 20:52:47 +0530

    Home page logo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,45 @@
 <template>
-  <div class="home">
-    <v-img
-      contain
-      :aspect-ratio="1/1"
-      position="top center"
-      src="../assets//Wikipedia_wordmark.png"
-    />
-  </div>
+  <v-layout
+    justify-center
+    align-center
+  >
+    <v-card class="my-5">
+      <v-card-title>
+        <h1 align="center">
+          <v-img
+            :aspect-ratio="1/1"
+            contain
+            height="3em"
+            width="3em"
+            src="@/assets/Wikipedia logo version 2.svg?lazy"
+          /> <span class="cap">W</span><span class="smallcap">ikipedi</span><span class="cap">A</span>
+        </h1>
+      </v-card-title>
+      <v-card-actions
+        class="my-5"
+      >
+        <v-btn
+          text
+          to="page/random"
+        >
+          Read Random Article
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-layout>
 </template>
-
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+<style scoped lang="less" >
+h1 {
+  font-size: 3em;
+  font-family: "Libertinus serif" !important;
+  font-feature-settings: "salt";
+  .smallcap {
+    font-variant: common-ligatures small-caps;
+  }
+}
+</style>
