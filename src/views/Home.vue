b87d02ed68af90b81962a9b2d462ffe72701e4ba
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2019-09-18 23:32:21 +0530

    Home page cards

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,46 +1,79 @@
 <template>
   <v-layout
-    justify-center
-    align-center
     row
-    fill-height
+    wrap
+    class="px-auto mx-auto"
+    align-center
   >
     <v-card
-      class="my-5"
-      :elevation="0"
+      width="30%"
+      max-width="400px"
+      min-width="400px"
+      :to="`/page/${contentLanguage}/${article.title}`"
+      class="ma-2  overflow-y-hidden"
+      height="50vh"
+      v-for="article in mostreadArticles"
+      :key="article.pageid"
     >
+      <v-img
+        v-if="article.thumbnail"
+        :src="article.thumbnail.source"
+        cover
+        aspect-ratio="2"
+      />
+
       <v-card-title>
-        <h1>
-          <span class="cap">W</span><span class="smallcap">ikipedi</span><span class="cap">A</span>
-        </h1>
+        <h3
+          class="headline mb-0"
+          v-html="article.displaytitle"
+        />
       </v-card-title>
-      <v-card-actions
-        class="my-5"
-      >
-        <v-btn
-          text
-          to="page/random"
-          style="width: 100%;"
-        >
-          Read a random Article
-        </v-btn>
-      </v-card-actions>
+      <v-card-text class="extract text-xs-left overflow-y-hidden mb-2 pb-2">
+        <h4>
+          {{ article.description }}
+        </h4>
+        <p
+          v-html="article.extract_html"
+        />
+      </v-card-text>
     </v-card>
   </v-layout>
 </template>
+
 <script>
+import axios from "axios";
+import { mapGetters, mapState, mapMutations } from "vuex";
+
 export default {
-  name: 'Home'
-}
-</script>
-<style scoped lang="less" >
-h1 {
-  font-size: 2.5em;
-  font-family: "Libertinus serif" !important;
-  font-feature-settings: "salt";
-  .smallcap {
-    font-variant: common-ligatures small-caps;
-    margin-left: -5px;
+  name: 'Home',
+  data:() => ({
+    mostreadArticles:[]
+    }),
+    computed:{
+      ...mapState({
+      contentLanguage: state => state.app.contentLanguage,
+      preview: state => state.preview
+    })
+    },
+      watch: {
+    contentLanguage: function() {
+       const today= new Date();
+      this.feed(today.getFullYear(), ('0'+today.getMonth()).slice(-2), ('0'+today.getDate()).slice(-2));
+    }
+      },
+  mounted: function(){
+      const today= new Date();
+      this.feed(today.getFullYear(), ('0'+today.getMonth()).slice(-2), ('0'+today.getDate()).slice(-2));
+  },
+  methods:{
+    feed(year, month, date){
+      const api=`https://${this.contentLanguage}.wikipedia.org/api/rest_v1/feed/featured/${year}/${month}/${date}`;
+      axios
+        .get(api)
+        .then(response => {
+          this.mostreadArticles=response.data.mostread.articles;
+        })
+    }
   }
 }
-</style>
+</script>
