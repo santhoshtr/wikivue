6ebb4e2e85a4794151ffcea57acee3b0ee2ded6d
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2019-09-19 09:50:46 +0530

    Home page cards - todays featured article

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,9 @@
 <template>
   <v-layout
-    row
+    justify-center
     wrap
-    class="px-auto mx-auto"
+    row
+    class="px-2 ma-2"
     align-center
   >
     <v-card
@@ -10,16 +11,15 @@
       max-width="400px"
       min-width="400px"
       :to="`/page/${contentLanguage}/${article.title}`"
-      class="ma-2  overflow-y-hidden"
+      class="ma-2 overflow-y-hidden"
       height="50vh"
-      v-for="article in mostreadArticles"
+      v-for="article in [...tfa, ...mostreadArticles]"
       :key="article.pageid"
     >
       <v-img
-        v-if="article.thumbnail"
-        :src="article.thumbnail.source"
+        :src="article.thumbnail? article.thumbnail.source:require('@/assets/Wikipedia logo version 2.svg?lazy')"
         cover
-        aspect-ratio="2"
+        aspect-ratio="2.5"
       />
 
       <v-card-title>
@@ -29,12 +29,8 @@
         />
       </v-card-title>
       <v-card-text class="extract text-xs-left overflow-y-hidden mb-2 pb-2">
-        <h4>
-          {{ article.description }}
-        </h4>
-        <p
-          v-html="article.extract_html"
-        />
+        <h4>{{ article.description }}</h4>
+        <p v-html="article.extract_html" />
       </v-card-text>
     </v-card>
   </v-layout>
@@ -45,35 +41,36 @@ import axios from "axios";
 import { mapGetters, mapState, mapMutations } from "vuex";
 
 export default {
-  name: 'Home',
-  data:() => ({
-    mostreadArticles:[]
-    }),
-    computed:{
-      ...mapState({
-      contentLanguage: state => state.app.contentLanguage,
-      preview: state => state.preview
+  name: "Home",
+  data: () => ({
+    mostreadArticles: [],
+    tfa: []
+  }),
+  computed: {
+    ...mapState({
+      contentLanguage: state => state.app.contentLanguage
     })
-    },
-      watch: {
-    contentLanguage: function() {
-       const today= new Date();
-      this.feed(today.getFullYear(), ('0'+today.getMonth()).slice(-2), ('0'+today.getDate()).slice(-2));
-    }
-      },
-  mounted: function(){
-      const today= new Date();
-      this.feed(today.getFullYear(), ('0'+today.getMonth()).slice(-2), ('0'+today.getDate()).slice(-2));
   },
-  methods:{
-    feed(year, month, date){
-      const api=`https://${this.contentLanguage}.wikipedia.org/api/rest_v1/feed/featured/${year}/${month}/${date}`;
-      axios
-        .get(api)
-        .then(response => {
-          this.mostreadArticles=response.data.mostread.articles;
-        })
+  watch: {
+    contentLanguage: function() {
+      this.feed();
+    }
+  },
+  mounted: function() {
+    this.feed();
+  },
+  methods: {
+    feed() {
+      const today = new Date();
+      const year = today.getFullYear();
+      const month = ("0" + (today.getMonth() + 1)).slice(-2);
+      const date = ("0" + today.getDate()).slice(-2);
+      const api = `https://${this.contentLanguage}.wikipedia.org/api/rest_v1/feed/featured/${year}/${month}/${date}`;
+      axios.get(api).then(response => {
+        this.mostreadArticles = response.data.mostread.articles;
+        this.tfa = response.data.tfa? [response.data.tfa] : [];
+      });
     }
   }
-}
+};
 </script>
