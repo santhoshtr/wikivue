7ff98e407120c73847d0d7f01ee99c05ca22d863
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2019-10-03 16:58:35 +0530

    Fix i18n fallback loading

--- src/main.js
+++ src/main.js
@@ -3,16 +3,27 @@ import App from './App.vue'
 import router from './router'
 import store from './store/'
 import './registerServiceWorker'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
 import i18n from 'vue-banana-i18n'
 
 Vue.config.productionTip = false
 
-const locale=navigator.language.split('-')[0] || 'en'
+const locale = navigator.language.split('-')[0] || 'en'
+const finalFallback = 'en'
+const messages = {
+  [finalFallback]: require(`@/assets/i18n/${finalFallback}.json`),
+}
+
+try {
+  messages[locale] = require(`@/assets/i18n/${locale}.json`)
+} catch {
+  // Not localised. We have loaded fallback locale.
+}
+
 Vue.use(i18n, {
   locale,
   finalFallback: 'en',
-  messages: { }
+  messages
 })
 
 new Vue({
@@ -20,15 +31,12 @@ new Vue({
   store,
   vuetify,
   render: h => h(App),
-  created () {
+  created() {
     if (sessionStorage.redirect) {
       const redirect = sessionStorage.redirect
       delete sessionStorage.redirect
       this.$router.push(redirect)
     }
-    import( `./assets/i18n/${this.i18n.locale}`).then(messages => {
-      this.i18n.loadMessages (messages.default, this.i18n.locale);
-    })
-    this.$store.commit('app/setUiLanguage',locale)
+    this.$store.commit('app/setUiLanguage', locale)
   }
 }).$mount('#app')
