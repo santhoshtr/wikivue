832d9a9cf1b6266f63b32b72e2a056ad064e1cfe
Subin Siby <mail@subinsb.com>
2020-07-30 15:20:36 +0530

    Use v-btn instead of router-link

--- src/components/Brand.vue
+++ src/components/Brand.vue
@@ -1,9 +1,9 @@
 <template>
   <v-toolbar-title class="wikipedia" v-show="$vuetify.breakpoint.smAndUp">
-    <router-link to="/">
+    <v-btn x-large text to="/">
       <span class="cap">W</span><span class="smallcap">ikipedi</span
       ><span class="cap">A</span>
-    </router-link>
+    </v-btn>
   </v-toolbar-title>
 </template>
 
@@ -21,9 +21,5 @@ export default {
     font-variant: common-ligatures small-caps;
     margin-left: -2px;
   }
-}
-.v-application .wikipedia a {
-  color: #000;
-  text-decoration: none;
 }
 </style>
